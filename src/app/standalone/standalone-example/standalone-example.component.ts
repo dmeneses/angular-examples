4039f47384e08b00137d418ace0cc589d7e2014c
Daniela Meneses <daniela.meneses@enverus.com>
2024-12-03 01:16:48 -0700

    Added documentation

--- src/app/standalone/standalone-example/standalone-example.component.ts
+++ src/app/standalone/standalone-example/standalone-example.component.ts
@@ -8,12 +8,13 @@ import {DogListComponent} from '../dog-list/dog-list.component';
   // Do not need to be declared in an NgModule.
   standalone: true,
   // Standalone components specify their dependencies directly instead of getting them through NgModules.
-  // For example, if StandaloneExampleComponent is a standalone component, it can directly import another standalone component DogListComponent.
+  // Given that StandaloneExampleComponent is a standalone component, it can
+  // - Import another standalone component: DogListComponent.
+  // - Import an existing module directly: CommonModule
   imports: [CommonModule, DogListComponent],
   templateUrl: './standalone-example.component.html',
   styleUrl: './standalone-example.component.scss'
 })
 export class StandaloneExampleComponent {
-
 }
 
