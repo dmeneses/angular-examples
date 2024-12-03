53cf48c3031a3d74a33a90218d14fadcd5b74c34
Daniela Meneses <daniela.meneses@enverus.com>
2024-12-03 01:24:13 -0700

    Example of standalone with modules

---
+++ src/app/signals/signals.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SignalsExampleComponent} from './signals-example/signals-example.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    // Standalone components can also be imported into existing NgModules-based contexts.
+    // This allows existing applications (which are using NgModules today) to incrementally adopt the new, standalone style of component.
+    // You can import a standalone component (or directive, or pipe) just like you would an NgModule
+    SignalsExampleComponent
+  ],
+  exports: [
+    SignalsExampleComponent
+  ]
+})
+export class SignalsModule { }
